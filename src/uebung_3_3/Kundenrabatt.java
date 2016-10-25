bf955fcca99d9df2e0e4c9382b5a5acaca6a2243
SaeschM <sascha.mangelsen@web.de>
2016-10-25 16:51:09 +0200

    Kundenrabatt Übung 3_3

---
+++ src/uebung_3_3/Kundenrabatt.java
@@ -0,0 +1,188 @@
+package uebung_3_3;
+
+public class Kundenrabatt {
+
+	public static void main(String[] args) {
+		double alter = 20;
+		double bmi = 0;
+		double größeInMM = 185;
+		double gewicht = 85;
+		boolean istFrau = true;
+
+		bmi = gewicht / ((größeInMM / 100) * (größeInMM / 100));
+
+		System.out.println("Ihr BMI beträgt " + bmi);
+
+		if (istFrau = true) {
+			if (alter <= 24) {
+				if (bmi < 19) {
+					System.out.println("BMI ist zu niedrig");
+				} else {
+					if (bmi > 24) {
+						System.out.println("BMI ist zu hoch");
+					} else {
+						System.out.println("BMI ist gut");
+					}
+				}
+			}
+		} else {
+			if (alter < 24) {
+				if (bmi < 20) {
+					System.out.println("BMI ist zu niedrig");
+				} else {
+					if (bmi > 25) {
+						System.out.println("BMI ist zu hoch");
+					} else {
+						System.out.println("BMI ist gut");
+					}
+				}
+			}
+		}
+			
+			if (istFrau = true) {
+				if (alter > 24) {
+					if (alter < 35) {
+
+						if (bmi < 20) {
+							System.out.println("BMI ist zu niedrig");
+						} else {
+							if (bmi > 25) {
+								System.out.println("BMI ist zu hoch");
+							} else {
+								System.out.println("BMI ist gut");
+							}
+						}
+					}
+				}
+				} else {
+
+					if (bmi < 21) {
+						System.out.println("BMI ist zu niedrig");
+					} else {
+						if (bmi > 26) {
+							System.out.println("BMI ist zu hoch");
+						} else {
+							System.out.println("BMI ist gut");
+						}
+					}
+
+				}
+
+			if (istFrau = true) {
+				if (alter > 34) {
+					if (alter < 45) {
+
+						if (bmi < 21) {
+							System.out.println("BMI ist zu niedrig");
+						} else {
+							if (bmi > 26) {
+								System.out.println("BMI ist zu hoch");
+							} else {
+								System.out.println("BMI ist gut");
+							}
+						}
+					}
+				}
+				} else {
+
+					if (bmi < 22) {
+						System.out.println("BMI ist zu niedrig");
+					} else {
+						if (bmi > 27) {
+							System.out.println("BMI ist zu hoch");
+						} else {
+							System.out.println("BMI ist gut");
+						}
+					}
+
+				}
+			
+			
+			if (istFrau = true) {
+				if (alter > 44) {
+					if (alter < 55) {
+
+						if (bmi < 22) {
+							System.out.println("BMI ist zu niedrig");
+						} else {
+							if (bmi > 27) {
+								System.out.println("BMI ist zu hoch");
+							} else {
+								System.out.println("BMI ist gut");
+							}
+						}
+					}
+				}
+				} else {
+
+					if (bmi < 23) {
+						System.out.println("BMI ist zu niedrig");
+					} else {
+						if (bmi > 28) {
+							System.out.println("BMI ist zu hoch");
+						} else {
+							System.out.println("BMI ist gut");
+						}
+					}
+
+				}
+			
+			if (istFrau = true) {
+				if (alter > 54) {
+					if (alter < 65) {
+
+						if (bmi < 23) {
+							System.out.println("BMI ist zu niedrig");
+						} else {
+							if (bmi > 28) {
+								System.out.println("BMI ist zu hoch");
+							} else {
+								System.out.println("BMI ist gut");
+							}
+						}
+					}
+				}
+				} else {
+
+					if (bmi < 24) {
+						System.out.println("BMI ist zu niedrig");
+					} else {
+						if (bmi > 29) {
+							System.out.println("BMI ist zu hoch");
+						} else {
+							System.out.println("BMI ist gut");
+						}
+					}
+
+				}
+			if (istFrau = true) {
+				if (alter > 64) {
+					
+
+						if (bmi < 24) {
+							System.out.println("BMI ist zu niedrig");
+						} else {
+							if (bmi > 29) {
+								System.out.println("BMI ist zu hoch");
+							} else {
+								System.out.println("BMI ist gut");
+							}
+						}
+					}
+				}
+				 else {
+
+					if (bmi < 25) {
+						System.out.println("BMI ist zu niedrig");
+					} else {
+						if (bmi > 30) {
+							System.out.println("BMI ist zu hoch");
+						} else {
+							System.out.println("BMI ist gut");
+						}
+					}
+
+				}
+			
+			}
+		}
